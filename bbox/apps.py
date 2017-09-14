369b4b1671502736acdbbb210ffce503db1976d8
SheyDenis <denis.sheyer@gmail.com>
2017-09-14 16:46:40 +0300

    Should work. Probably won't.

--- bbox/apps.py
+++ bbox/apps.py
@@ -2,4 +2,19 @@ from django.apps import AppConfig
 
 
 class BboxConfig(AppConfig):
-    name = 'bbox'
+	name = 'bbox'
+
+	def ready(self):
+		import socket
+		from zeroconf import ServiceInfo
+		from zeroconf import Zeroconf
+		ip = socket.inet_aton(socket.gethostbyname(socket.gethostname()))
+		port = 9998  # TODO - Port number?
+		info = ServiceInfo(_type="_http._tcp.local.", name="FatCat_BrainBox", address=ip, port=port, properties=None,
+			server=None)
+		zeroconf = Zeroconf()
+		# TODO - Log this to systemlog
+		zeroconf.register_service(info)
+
+	# zeroconf.unregister_service(info)
+	# zeroconf.close()
