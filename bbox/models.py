81fa3b54085301724a8e16b98787f8ebee19923a
Shabnata <mer.natalie@gmail.com>
2017-09-14 12:29:00 +0300

    Creation of DB file from Models use 2 commands: python manage.py makemigrations bbox python manage.py migrate

--- bbox/models.py
+++ bbox/models.py
@@ -5,58 +5,59 @@ from enum import Enum
 
 
 class Account(models.Model):
-    user_name = models.TextField(unique=True)
-    password = models.TextField()
+	user_name = models.TextField(unique=True)
+	password = models.TextField()
 
-    class Meta:
-        managed = False
-        db_table = 'account'
+	class Meta:
+		managed = True
+		db_table = 'account'
 
 
 class FeedingLog(models.Model):
-    box_id = models.TextField()
-    feeding_id = models.TextField(unique=True)
-    card_id = models.TextField()
-    open_time = models.TextField()  # This field type is a guess.
-    close_time = models.TextField()  # This field type is a guess.
-    start_weight = models.TextField()  # This field type is a guess.
-    end_weight = models.TextField()  # This field type is a guess.
-    synced = models.IntegerField()
+	box_id = models.TextField()
+	feeding_id = models.TextField(unique=True)
+	card_id = models.TextField()
+	open_time = models.TextField()  # This field type is a guess.
+	close_time = models.TextField()  # This field type is a guess.
+	start_weight = models.TextField()  # This field type is a guess.
+	end_weight = models.TextField()  # This field type is a guess.
+	synced = models.IntegerField()
 
-    class Meta:
-        managed = False
-        db_table = 'feeding_logs'
+	class Meta:
+		managed = True
+		db_table = 'feeding_logs'
 
 
 class FoodBox(models.Model):
-    box_id = models.TextField(unique=True)
-    box_ip = models.TextField()
-    box_name = models.TextField()
-    box_last_sync = models.TextField()
+	box_id = models.TextField(unique=True)
+	box_ip = models.TextField()
+	box_name = models.TextField()
+	box_last_sync = models.TextField()
 
-    class Meta:
-        managed = False
-        db_table = 'food_boxes'
+	class Meta:
+		managed = True
+		db_table = 'food_boxes'
 
 
 class SystemLog(models.Model):
-    time_stamp = models.TextField()  # This field type is a guess.
-    message = models.TextField(blank=True, null=True)
-    message_type = models.TextField()
-    severity = models.IntegerField()
+	time_stamp = models.TextField()  # This field type is a guess.
+	message = models.TextField(blank=True, null=True)
+	message_type = models.TextField()
+	severity = models.IntegerField()
 
-    class Meta:
-        managed = False
-        db_table = 'system_logs'
+	class Meta:
+		managed = True
+		db_table = 'system_logs'
 
 
 class SystemSettings(models.Model):
-    key_name = models.TextField(unique=True)
-    value_text = models.TextField(blank=True, null=True)
+	key_name = models.TextField(unique=True)
+	value_text = models.TextField(blank=True, null=True)
 
-    class Meta:
-        managed = False
-        db_table = 'system_settings'
+	class Meta:
+		managed = True
+		db_table = 'system_settings'
+
 
 class MessageTypes(Enum):
 	Information = 1  # General information
