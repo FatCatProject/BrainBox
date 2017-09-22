abc17b14b5b4595e2b5f1aa5eda3f587a2fa6ee9
SheyDenis <denis.sheyer@gmail.com>
2017-09-22 18:34:59 +0300

    A very (very...) basic index place holder and place holders for the header and footer.
    Also added Bootstrap4 and jQuery files.

--- BrainBox/settings.py
+++ BrainBox/settings.py
@@ -28,23 +28,32 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = ['bbox.apps.BboxConfig', 'django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
-				  'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles', ]
+INSTALLED_APPS = [
+	'bbox.apps.BboxConfig', 'django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
+	'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles', 'web_ui.apps.WebUiConfig'
+]
 
-MIDDLEWARE = ['django.middleware.security.SecurityMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware',
-			  'django.middleware.common.CommonMiddleware', 'django.middleware.csrf.CsrfViewMiddleware',
-			  'django.contrib.auth.middleware.AuthenticationMiddleware',
-			  'django.contrib.messages.middleware.MessageMiddleware',
-			  'django.middleware.clickjacking.XFrameOptionsMiddleware', ]
+MIDDLEWARE = [
+	'django.middleware.security.SecurityMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware', 'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware', 'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware'
+]
 
 ROOT_URLCONF = 'BrainBox.urls'
 
 TEMPLATES = [
-	{'BACKEND': 'django.template.backends.django.DjangoTemplates', 'DIRS': [], 'APP_DIRS': True,
-	 'OPTIONS': {'context_processors': [
-		 'django.template.context_processors.debug', 'django.template.context_processors.request',
-		 'django.contrib.auth.context_processors.auth',
-		 'django.contrib.messages.context_processors.messages', ], }, }, ]
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates', 'DIRS': [], 'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug', 'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
+]
 
 WSGI_APPLICATION = 'BrainBox.wsgi.application'
 
