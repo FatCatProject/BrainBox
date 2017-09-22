b89cd2f44026c76d9fae2d9e560b147c83298030
SheyDenis <denis.sheyer@gmail.com>
2017-09-22 18:34:59 +0300

    A very (very...) basic index place holder and place holders for the header and footer.
    Also added Bootstrap4 and jQuery files.

--- web_ui/views.py
+++ web_ui/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+	return render(request, template_name="web_ui/index.html")
+
+# from django.core import serializers
+# data = serializers.serialize("xml", SomeModel.objects.all())
+# XMLSerializer = serializers.get_serializer("xml")
+# xml_serializer = XMLSerializer()
+# xml_serializer.serialize(queryset)
+# data = xml_serializer.getvalue()
