0d30e80a9d0c20b7a7322cf40a4253018f8d2bc6
SheyDenis <denis.sheyer@gmail.com>
2017-09-23 04:36:12 +0300

    Remove unused imports in BrainBoxDB. Change get_x methods in BrainBoxDB to return tuples and not QuerySet. Make all methods in BrainBoxDB to be static methods. Remove un needed sqlite3.connect and cursor in BrainBoxDB. Correct set_system_setting method in BrainBoxDBto not raise an exception when setting a setting that doesn't exists yet.
    Change the way a datetime was added in myfunc == "add_feeding_log"
    and myfunc == "add_system_log" to an actual datetime

--- bbox/bboxDB.py
+++ bbox/bboxDB.py
@@ -1,138 +1,148 @@
-import os.path
-import sqlite3
-import time
-import datetime
 from .models import FeedingLog, Account, FoodBox, SystemLog, SystemSetting
 
 
 class BrainBoxDB:
-	def __init__(self):
-		# Check if DB exists , if not - create it
-		self.conn = sqlite3.connect('db.sqlite3')
-		self.c = self.conn.cursor()  # type: sqlite3.Cursor
-		self.c.execute("PRAGMA foreign_keys = ON")
-
-	def __del__(self):
-		self.c.close()
-		self.conn.close()
-
 	### Start feeding_logs functiones ###
-	def get_feeding_log_by_id(self, logID: str):
+
+	@staticmethod
+	def get_feeding_log_by_id(logID: str):
 		"""
 		Function gets a feeding_log ID (The original UUID) and returns a feeding_log OBJECT
 		"""
-		return FeedingLog.objects.filter(feeding_id=logID)
+		return FeedingLog.objects.filter(feeding_id=logID).first()
 
-	def get_all_feeding_logs(self):
+	@staticmethod
+	def get_all_feeding_logs():
 		"""
 		Returns a tuple of all feeding logs
 		"""
-		return FeedingLog.objects.all()
+		return tuple(FeedingLog.objects.all())
 
-	def get_synced_feeding_logs(self):
+	@staticmethod
+	def get_synced_feeding_logs():
 		"""
 		Returns a tuple of synced feeding logs
 		"""
-		return FeedingLog.objects.filter(synced=1)
+		return tuple(FeedingLog.objects.filter(synced=1))
 
-	def get_not_synced_feeding_logs(self):
+	@staticmethod
+	def get_not_synced_feeding_logs():
 		"""
 		Returns a tuple of synced feeding logs
 		"""
-		return FeedingLog.objects.filter(synced=0)
+		return tuple(FeedingLog.objects.filter(synced=0))
 
-	def set_feeding_log_synced(self, myLogUID: str):
+	@staticmethod
+	def set_feeding_log_synced(myLogUID: str):
 		"""
 		Get feeding_log UID and change the synced status to True = 1
 		"""
 		FeedingLog.objects.filter(feeding_id=myLogUID).update(synced=1)
 
-	def set_feeding_log_not_synced(self, myLogUID: str):
+	@staticmethod
+	def set_feeding_log_not_synced(myLogUID: str):
 		"""
 		Get feeding_log UID and change the synced status to False = 0
 		"""
 		FeedingLog.objects.filter(feeding_id=myLogUID).update(synced=0)
 
-	def delete_synced_feeding_logs(self):
+	@staticmethod
+	def delete_synced_feeding_logs():
 		"""
 		Delete all synced feeding_logs from the DB
 		"""
 		FeedingLog.objects.filter(synced=1).delete()
 
-	def get_feeding_logs_by_box_id(self, boxId):
-		return FeedingLog.objects.filter(box_id=boxId)
+	@staticmethod
+	def get_feeding_logs_by_box_id(boxId):
+		return tuple(FeedingLog.objects.filter(box_id=boxId))
 
-	def add_feeding_log(self, myLog: FeedingLog):
+	@staticmethod
+	def add_feeding_log(myLog: FeedingLog):
 		"""
 		Getting a feeding_log as an object and Adding it to the DB
 		"""
-		FeedingLog.objects.create(box_id=myLog.box_id,
-								  feeding_id=myLog.feeding_id,
-								  card_id=myLog.card_id,
-								  open_time=myLog.open_time,
-								  close_time=myLog.close_time,
-								  start_weight=myLog.start_weight,
-								  end_weight=myLog.end_weight,
-								  synced=myLog.synced)
+		FeedingLog.objects.create(
+			box_id=myLog.box_id,
+			feeding_id=myLog.feeding_id,
+			card_id=myLog.card_id,
+			open_time=myLog.open_time,
+			close_time=myLog.close_time,
+			start_weight=myLog.start_weight,
+			end_weight=myLog.end_weight,
+			synced=myLog.synced
+		)
 
 	### END of feeding_logs functiones ###
 
 	### Start of system_logs functions ###
-	def get_system_log_by_id(self, logID: int):
+
+	@staticmethod
+	def get_system_log_by_id(logID: int):
 		"""
 		Function gets a SystemLog ID and returns a SystemLog object or None if no such object
 		"""
-		return SystemLog.objects.filter(rowid=logID)
+		return SystemLog.objects.filter(rowid=logID).first()
 
-	def get_all_system_logs(self):
+	@staticmethod
+	def get_all_system_logs():
 		"""Returns a tuple of all system logs since logs_since, or all of them
 		"""
-		return SystemLog.objects.filter()
+		return tuple(SystemLog.objects.all())
 
-	def add_system_log(self, myLog: SystemLog):
+	@staticmethod
+	def add_system_log(myLog: SystemLog):
 		"""
 		Function gets a SystemLog object and writes it to the database
 		"""
-		SystemLog.objects.create(time_stamp=myLog.time_stamp,
-								 message=myLog.message,
-								 message_type=myLog.message_type,
-								 severity=myLog.severity)
+		SystemLog.objects.create(
+			time_stamp=myLog.time_stamp,
+			message=myLog.message,
+			message_type=myLog.message_type,
+			severity=myLog.severity
+		)
 
 	### End of system_logs functions ###
 
 	### Start of system_settings functiones ###
-	def get_system_setting(self, setting: str):
+
+	@staticmethod
+	def get_system_setting(setting: str):
 		"""Function returns a value of a specific setting from enums that are available:
 			the input must be "SystemSettings.key_name"
 			key names: BrainBox_ID / BrainBox_Name / Sync_Interval
 			If the key_name is still not written in the DB or has no value it returns None
 		"""
-		return SystemSetting.objects.filter(key_name=setting)
+		return SystemSetting.objects.filter(key_name=setting).first()
 
-	def set_system_setting(self, setting: str, value: str):
+	@staticmethod
+	def set_system_setting(setting: str, value: str):
 		"""
 		Function sets a value to a key in "system_settings" table
 		If the key doesn't exist yet , it will write a new row with the key and value
 		"""
-		SystemSetting.objects.filter(key_name=setting).update(value_text=value)
+		SystemSetting.objects.update_or_create(defaults={"value_text": value}, key_name=setting)
 
 	### END of system_logs functions ###
 
 	### Start of Account functions ###
-	def get_account_info(self):
+
+	@staticmethod
+	def get_account_info():
 		"""
 		Returns ALL account info: username&password
 		"""
-		return Account.objects.all()
+		return tuple(Account.objects.all())
 
-	def add_account(self, account: Account):
+	@staticmethod
+	def add_account(account: Account):
 		"""
 		add a new account to DB
 		"""
-		Account.objects.create(user_name=account.user_name,
-							   password=account.password)
+		Account.objects.create(user_name=account.user_name, password=account.password)
 
-	def change_user_name_and_or_password(self, acc: Account, new_user_name: str = None, new_password: str = None):
+	@staticmethod
+	def change_user_name_and_or_password(acc: Account, new_user_name: str = None, new_password: str = None):
 		"""
 		Function receives an account and changes user_name and/or password
 		"""
@@ -142,24 +152,29 @@ class BrainBoxDB:
 		if new_user_name is not None:
 			Account.objects.filter(user_name=currentAccount.user_name).update(user_name=new_user_name)
 
-		### END of Account functions ###
+	### END of Account functions ###
 
 	### Start of FoodBox functions ###
-	def get_all_foodBoxes(self):
+
+	@staticmethod
+	def get_all_foodBoxes():
 		"""
 		Returns a tuple of all food_boxes
 		"""
-		return FoodBox.objects.all()
+		return tuple(FoodBox.objects.all())
 
-	def delete_foodBox(self, foodBox: FoodBox):
+	@staticmethod
+	def delete_foodBox(foodBox: FoodBox):
 		"""
 		Delete a foodbox from the table
 		"""
 		FoodBox.objects.filter(box_id=foodBox.box_id).delete()
 
-	def get_foodBox_by_foodBox_id(self, boxId: str):
+	@staticmethod
+	def get_foodBox_by_foodBox_id(boxId: str):
 		"""
 		Returns a specific foodbox by ID
 		"""
-		return FoodBox.objects.filter(box_id=boxId)
-		### END of FoodBox functions ###
+		return FoodBox.objects.filter(box_id=boxId).first()
+
+	### END of FoodBox functions ###
--- bbox/views.py
+++ bbox/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpRequest
 import time
+import datetime
 from django.utils import timezone
 
 from .models import FeedingLog, Account, SystemLog, SystemSetting, FoodBox
@@ -10,8 +11,7 @@ from bbox.bboxDB import BrainBoxDB
 
 
 def get_feeding_log_by_id(request, id):
-	db = BrainBoxDB()
-	mylog = db.get_feeding_log_by_id(logID=id)
+	mylog = BrainBoxDB.get_feeding_log_by_id(logID=id)
 	return HttpResponse(mylog)
 
 
@@ -28,106 +28,98 @@ def accounts(request):
 
 
 def get_all_feeding_logs(request):
-	db = BrainBoxDB()
-	mylogs = db.get_all_feeding_logs()
+	mylogs = BrainBoxDB.get_all_feeding_logs()
 	return HttpResponse(mylogs)
 
 
 def get_synced_feeding_logs(request):
-	db = BrainBoxDB()
-	mylogs = db.get_synced_feeding_logs()
+	mylogs = BrainBoxDB.get_synced_feeding_logs()
 	return HttpResponse(mylogs)
 
 
 def get_not_synced_feeding_logs(request):
-	db = BrainBoxDB()
-	mylogs = db.get_not_synced_feeding_logs()
+	mylogs = BrainBoxDB.get_not_synced_feeding_logs()
 	return HttpResponse(mylogs)
 
 
 def set_feeding_log_synced(request, id):
-	db = BrainBoxDB()
-	mylog = db.set_feeding_log_synced(id)
+	mylog = BrainBoxDB.set_feeding_log_synced(id)
 	return HttpResponse(mylog)
 
 
 def set_feeding_log_not_synced(request, id):
-	db = BrainBoxDB()
-	mylog = db.set_feeding_log_not_synced(id)
+	mylog = BrainBoxDB.set_feeding_log_not_synced(id)
 	return HttpResponse(mylog)
 
 
 def delete_synced_feeding_logs(request):
-	db = BrainBoxDB()
-	mylogs = db.delete_synced_feeding_logs()
+	mylogs = BrainBoxDB.delete_synced_feeding_logs()
 	return HttpResponse(mylogs)
+
 
 def add_feeding_log(request, myLog):
-	db = BrainBoxDB()
-	mylog = db.add_feeding_log(myLog)
+	mylog = BrainBoxDB.add_feeding_log(myLog)
 	return HttpResponse(mylog)
+
 
 def get_system_log_by_id(request, id):
-	db = BrainBoxDB()
-	mylog = db.get_system_log_by_id(logID=id)
+	mylog = BrainBoxDB.get_system_log_by_id(logID=id)
 	return HttpResponse(mylog)
+
 
 def get_all_system_logs(request):
-	db = BrainBoxDB()
-	mylogs = db.get_all_system_logs()
+	mylogs = BrainBoxDB.get_all_system_logs()
 	return HttpResponse(mylogs)
 
+
 def add_system_log(request, myLog):
-	db = BrainBoxDB()
-	mylog = db.add_system_log(myLog)
+	mylog = BrainBoxDB.add_system_log(myLog)
 	return HttpResponse(mylog)
+
 
 def get_system_setting(request, setting):
-	db = BrainBoxDB()
-	mylog = db.get_system_setting(setting)
+	mylog = BrainBoxDB.get_system_setting(setting)
 	return HttpResponse(mylog)
 
+
 def set_system_setting(request, setting, val):
-	db = BrainBoxDB()
-	mylog = db.set_system_setting(setting, val)
+	mylog = BrainBoxDB.set_system_setting(setting, val)
 	return HttpResponse(mylog)
 
 
 def get_account_info(request):
-	db = BrainBoxDB()
-	myAccount = db.get_account_info()
+	myAccount = BrainBoxDB.get_account_info()
 	return HttpResponse(myAccount)
 
+
 def add_account(request, account):
-	db = BrainBoxDB()
-	mylog = db.add_account(account)
+	mylog = BrainBoxDB.add_account(account)
 	return HttpResponse(mylog)
 
+
 def change_user_name_and_or_password(request, acc, user_name, password):
-	db = BrainBoxDB()
-	mylog = db.change_user_name_and_or_password(acc, user_name, password)
+	mylog = BrainBoxDB.change_user_name_and_or_password(acc, user_name, password)
 	return HttpResponse(mylog)
 
 
 def get_all_foodBoxes(request):
-	db = BrainBoxDB()
-	mylogs = db.get_all_foodBoxes()
+	mylogs = BrainBoxDB.get_all_foodBoxes()
 	return HttpResponse(mylogs)
+
 
 def delete_foodBox(request, myBox):
-	db = BrainBoxDB()
-	mylogs = db.delete_foodBox(myBox)
+	mylogs = BrainBoxDB.delete_foodBox(myBox)
 	return HttpResponse(mylogs)
 
+
 def get_foodBox_by_foodBox_id(request, id):
-	db = BrainBoxDB()
-	mylog = db.get_foodBox_by_foodBox_id(id)
+	mylog = BrainBoxDB.get_foodBox_by_foodBox_id(id)
 	return HttpResponse(mylog)
 
 
 def test(request: HttpRequest):
 	myfunc = request.GET.get("func")
-	db = BrainBoxDB()
+
 	if myfunc == "allFeedingLogs":
 		return allFeedingLogs(request)
 
@@ -154,14 +146,25 @@ def test(request: HttpRequest):
 
 	if myfunc == "add_feeding_log":
 		box_id = FoodBox.objects.get(pk=2)
-		myLog = FeedingLog(box_id=box_id,
-								  feeding_id='2a27f997dc8f47499623d125f1f4b4df',
-								  card_id='138-236-209-167-000',
-								  open_time=time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(1503402679)),
-								  close_time=time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(1503402679)),
-								  start_weight='3',
-								  end_weight='3',
-								  synced= False)
+		open_t = time.localtime(1503402679)  # type: time.struct_time
+		close_t = time.localtime(1503402679)  # type: time.struct_time
+		myLog = FeedingLog(
+			box_id=box_id,
+			feeding_id='2a27f997dc8f47499623d125f1f4b4df',
+			card_id='138-236-209-167-000',
+			open_time=datetime.datetime(
+				open_t.tm_year, open_t.tm_mon, open_t.tm_mday, open_t.tm_hour, open_t.tm_min, open_t.tm_sec,
+				tzinfo=datetime.timezone(offset=datetime.timedelta())  # This basically means "UTC == Local Time"
+
+			),
+			close_time=datetime.datetime(
+				close_t.tm_year, close_t.tm_mon, close_t.tm_mday, close_t.tm_hour, close_t.tm_min, close_t.tm_sec,
+				tzinfo=datetime.timezone(offset=datetime.timedelta())  # This basically means "UTC == Local Time"
+			),
+			start_weight='3',
+			end_weight='3',
+			synced=False
+		)
 		return add_feeding_log(request, myLog)
 
 	if myfunc == "get_system_log_by_id":
@@ -171,10 +174,17 @@ def test(request: HttpRequest):
 		return get_all_system_logs(request)
 
 	if myfunc == "add_system_log":
-		myLog = SystemLog(time_stamp=time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(1503402679)),
-								 message='msgNew',
-								 message_type='Information',
-								 severity=3)
+		log_time = time.localtime(1503402679)  # type: time.struct_time
+		print(log_time)
+		myLog = SystemLog(
+			time_stamp=datetime.datetime(
+				log_time.tm_year, log_time.tm_mon, log_time.tm_mday, log_time.tm_hour, log_time.tm_min, log_time.tm_sec,
+				tzinfo=datetime.timezone(offset=datetime.timedelta())  # This basically means "UTC == Local Time"
+			),
+			message='msgNew',
+			message_type='Information',
+			severity=3
+		)
 		return add_system_log(request, myLog)
 
 	if myfunc == "get_system_setting":
@@ -190,14 +200,15 @@ def test(request: HttpRequest):
 		return get_account_info(request)
 
 	if myfunc == "add_account":
-		acc = Account(user_name='Kot',password='Kot')
+		# Why not just Account.objects.create(user_name='Kot', password='Kot') then?
+		acc = Account(user_name='Kot', password='Kot')
 		return add_account(request, acc)
 
 	if myfunc == "change_user_name_and_or_password":
 		user_name = 'kottt'
 		password = 'kottt'
-		acc=Account.objects.get(user_name='kot')
-		db.change_user_name_and_or_password(acc, user_name, password)
+		acc = Account.objects.get(user_name='kot')
+		BrainBoxDB.change_user_name_and_or_password(acc, user_name, password)
 		return HttpResponse("Changed")
 
 	if myfunc == "get_all_foodBoxes":
@@ -205,7 +216,7 @@ def test(request: HttpRequest):
 
 	if myfunc == "delete_foodBox":
 		myBox = FoodBox.objects.get(box_id='3')
-		return delete_foodBox(request,myBox)
+		return delete_foodBox(request, myBox)
 
 	if myfunc == "get_foodBox_by_foodBox_id":
 		return get_foodBox_by_foodBox_id(request, id='1')
