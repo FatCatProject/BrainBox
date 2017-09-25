41c99660ecb3a6b75fadaa5aa15fa58b1676cc7f
SheyDenis <denis.sheyer@gmail.com>
2017-09-25 15:28:02 +0300

    2017-09-25 15:30:00

--- web_ui/urls.py
+++ web_ui/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^login$', views.login, name='login'),
+	url(r'^register$', views.register, name='register'),
+	url(r'^logout$', views.register, name='logout')
 ]
