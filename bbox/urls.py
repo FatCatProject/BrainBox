58c40d7c85979ad4d95e131deca488eb87f13eee
SheyDenis <denis.sheyer@gmail.com>
2017-10-01 17:34:30 +0300

    Minor fix.

--- bbox/urls.py
+++ bbox/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
 	url(r'^test/$', views.test, ),
 	url(r'^pushlogs/$', views.pushlogs, name="pushlogs"),
 	url(r'^pullcards/(?P<box_id>[a-z0-9]{32})/$', views.pullcards, name="pullcards"),
-	url(r'^pullfoodbox/(?P<box_id>[a-z0-9]{32})/$', views.pullfoodbox(), name="pullfoodbox")
+	url(r'^pullfoodbox/(?P<box_id>[a-z0-9]{32})/$', views.pullfoodbox, name="pullfoodbox")
 ]
