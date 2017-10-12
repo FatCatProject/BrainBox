83aa699294f2b56d6d5cdfe10ecdc6fe4f4ae197
SheyDenis <denis.sheyer@gmail.com>
2017-10-12 16:09:29 +0300

    Initial version of get_foodboxes server_task.

--- server_tasks/receive.py
+++ server_tasks/receive.py
@@ -104,3 +104,63 @@ def get_cards():
 
 	now = time.time()
 	BrainBoxDB.set_system_setting("Server_Last_Sync", str(now))
+
+
+def get_foodboxes():
+	# TODO - SSL
+	my_account = BrainBoxDB.get_account_info()  # type: Account
+	server_address = BrainBoxDB.get_system_setting("Server_Address")  # type: SystemSetting
+	assert my_account is not None
+	assert server_address is not None
+
+	my_auth = (my_account.user_name, my_account.server_token)
+	print("my_auth: {0}".format(my_auth))  # TODO - Delete debug message
+
+	server_address = server_address.value_text
+	get_address = "http://{0}/bbox/get_foodboxes/".format(server_address)
+	print("GET address get_foodboxes: {0}".format(get_address))  # TODO - Delete debug message
+
+	now = time.time()
+	try:
+		server_response = requests.get(url=get_address, auth=my_auth)
+		foodboxes_response = json.loads(server_response.text)
+
+		for foodbox in foodboxes_response['foodboxes']:
+			try:
+				tmp_foodbox = FoodBox.objects.get(box_id=foodbox["foodbox_id"])  # type: FoodBox
+				tmp_foodbox.box_name = foodbox["foodbox_name"]
+				tmp_foodbox.synced = False
+				tmp_foodbox.save()
+			except FoodBox.DoesNotExist:
+				my_log = SystemLog(
+					time_stamp=now,
+					message="Server returned unexpected FoodBox: {0}".format(foodbox["foodbox_id"]),
+					message_type="Fatal",
+					severity=1
+				)
+				print("Writing SystemLog: {0}".format(my_log))  # TODO - Delete debug message
+				BrainBoxDB.add_system_log(myLog=my_log)
+
+		my_log = SystemLog(
+			time_stamp=now,
+			message="GET foodboxes from server succeeded.",
+			message_type="Information",
+			severity=0
+		)
+	except (json.decoder.JSONDecodeError, AttributeError) as e:
+		my_log = SystemLog(
+			time_stamp=now,
+			message="Server returned unexpected response: {0}".format(e.args),
+			message_type="Fatal",
+			severity=2
+		)
+	except Exception as e:
+		my_log = SystemLog(
+			time_stamp=now,
+			message="Failed to GET foodboxes from server - Exception: {0}".format(e.args),
+			message_type="Error",
+			severity=2
+		)
+
+	print("Writing SystemLog: {0}".format(my_log))  # TODO - Delete debug message
+	BrainBoxDB.add_system_log(myLog=my_log)
