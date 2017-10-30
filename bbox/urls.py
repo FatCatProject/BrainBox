b7467813f51d587ae8885ea5c57f4bc7a536ca9b
SheyDenis <denis.sheyer@gmail.com>
2017-10-30 14:35:25 +0200

    Closes #28

--- bbox/urls.py
+++ bbox/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
 	url(r'^pushlogs/$', views.pushlogs, name="pushlogs"),
-	url(r'^pullcards/(?P<box_id>[a-z0-9]{32})/$', views.pullcards, name="pullcards"),
-	url(r'^pullfoodbox/(?P<box_id>[a-z0-9]{32})/$', views.pullfoodbox, name="pullfoodbox")
+	url(
+		r'^pullcards/(?P<box_id>[a-z0-9]{32})/$',
+		views.pullcards, name="pullcards"
+	),
+	url(
+		r'^pullfoodbox/(?P<box_id>[a-z0-9]{32})/$',
+		views.pullfoodbox, name="pullfoodbox"
+	)
 ]
